39198ef103a4fae691d982572b85829c551c3900
yu_higashi <yu_higashi@septeniventures.co.jp>
2015-09-17 16:10:17 +0900

    add calculateForTurn

--- app/search_and_destroy.rb
+++ app/search_and_destroy.rb
@@ -2,6 +2,9 @@ require 'bundler/setup'
 require 'opencv'
 include OpenCV
 
+#両マーカーの中心点間の距離(メートル)を設定
+KMarkerInterval = 0.10
+
 def searchObject(cvmat)
   rows = []
   cols = []
@@ -17,24 +20,28 @@ def searchObject(cvmat)
 end
 
 class Robot
-  attr_accessor :location, :direction, :interval
-  def initialize(head_point,tail_point,interval)
+
+  attr_accessor :location, :direction, :marker_interval, :meter_per_dot
+
+  def initialize(head_point,tail_point,marker_interval)
     @location = CvPoint.new((head_point.x+tail_point.x)/2,(head_point.y+tail_point.y)/2)
     @direction = Math.atan2(tail_point.y-head_point.y,head_point.x-tail_point.x)
-    @interval = 
-    puts "head: #{head_point.x} #{head_point.y}, tail: #{tail_point.x} #{tail_point.y}"
-    puts "location: #{location.x} #{location.y}"
-    puts "direction: #{@direction}"
+    @marker_interval = marker_interval
+    @meter_per_dot = @marker_interval/(Math.hypot(tail_point.x-head_point.x,tail_point.y-head_point.y).round)
   end
 
-  def radianForTurn(distination)
+  # distination:回転させたい目標地点(CvPoint)  return [目標地点へ向くために必要な角度,目標地点への距離]
+  def calculateForTurn(distination)
     dist_direction = Math.atan2(@location.y-distination.y,distination.x-@location.x)
-    return dist_direction-@direction
+
+    return dist_direction-@direction, Math.hypot(@location.x-distination.x,@location.y-distination.y)*@meter_per_dot
   end
 
 end
 
 begin
+  #background_image = CvMat.load("xxx/xxx.jpg")
+  #camera_image = CvMat.load(""yyy/yyy.jpg)
   input_img = CvMat.load("bug_match/case1.jpg")
   marker_img = CvMat.load("markers/red.png")
 rescue
@@ -42,8 +49,7 @@ rescue
   exit
 end
 
-#diff_image = background_image.abs_diff(camera_image).not
-#diff_image = diff_image.threshold()
+#input_img = background_image.abs_diff(camera_image).not
 
 gray = input_img.BGR2GRAY
 gray_smooth = gray.smooth(CV_GAUSSIAN)
@@ -64,13 +70,11 @@ match.each_with_index do |circle,index|
 end
 
 gray_smooth = gray_smooth.threshold(30,255,CV_THRESH_BINARY)
-rows,cols = searchObject(gray_smooth)
 
-from = CvPoint.new(cols.min,rows.min)
-to = CvPoint.new(cols.max,rows.max)
-center = CvPoint.new((cols.max-cols.min)/2,(rows.max-rows.min)/2)
+black_rows,black_cols = searchObject(gray_smooth)
 
-bot = Robot.new(head_point,tail_point)
+enemy_center = CvPoint.new((black_cols.max-black_cols.min)/2,(black_rows.max-black_rows.min)/2)
 
-gray_smooth.rectangle!(from,to,:color => CvColor::Black, :thickness => 1)
-gray_smooth.save_image("match_image.png")
+bot = Robot.new(head_point,tail_point,KMarkerInterval)
+
+puts bot.calculateForTurn(enemy_center)
