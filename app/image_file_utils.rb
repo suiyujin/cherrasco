bd969995c984850884f2c7fd9c3d47395361f557
suiyujin <suiyujin04@gmail.com>
2015-09-25 20:19:14 +0900

    fix comment

--- app/image_file_utils.rb
+++ app/image_file_utils.rb
@@ -4,7 +4,7 @@ module ImageFileUtils
   LIMIT_NUM_DATA = 3
 
   def check_file_limit(image_file_dir, image_file_prefix)
-    # tmp/images/内の画像がn個より多い場合、最新のn個を残して破棄
+    # image_file_dir内の指定画像がn個より多い場合、最新のn個を残して破棄
     Dir.chdir(image_file_dir) do
       image_files = Dir.glob(image_file_prefix).sort
       if image_files.size > LIMIT_NUM_DATA
